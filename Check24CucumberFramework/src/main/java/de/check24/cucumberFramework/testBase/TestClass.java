2ad16e5e2beb830775fa96e67a870d5113f30784
srividyakun <srividya.ece@gmail.com>
2018-11-24 06:46:26 +0100

    Commiting for the 2nd time

--- Check24CucumberFramework/src/main/java/de/check24/cucumberFramework/testBase/TestClass.java
+++ Check24CucumberFramework/src/main/java/de/check24/cucumberFramework/testBase/TestClass.java
@@ -2,4 +2,17 @@ package de.check24.cucumberFramework.testBase;
 
 public class TestClass {
 
+	public static void main(String[] args) {
+		/**
+		 * /*
+	git add filename
+	git add .
+	
+	git commit -m"comments"
+	
+	git push
+	
+	*/
+		 System.out.println("testing for GIT");
+	}
 }
